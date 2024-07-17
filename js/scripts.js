e9e304dc4424624b4b7f72cbfa427c7bcb5c320c
Alex Lantz <aclantz321@gmail.com>
2024-07-17 14:34:41 -0400

    updates for 2 final projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,63 @@
-(function() {
-    let form = document.querySelector('.contact-form');
-    let emailInput = document.querySelector('#form-email');
-    let messageInput = document.querySelector('#form-message');
+(function () {
+  let form = document.querySelector(".contact-form");
+  let emailInput = document.querySelector("#form-email");
+  let messageInput = document.querySelector("#form-message");
 
-    function showErrorMessage(input, message) {
-        let container = input.parentElement;
+  function showErrorMessage(input, message) {
+    let container = input.parentElement;
 
-        let error = container.querySelector('.error-message');
-        if (error) {
-            container.removeChild(error);
-        }
-        if (message) {
-            let error = document.createElement('div');
-            error.classList.add('error-message');
-            error.innerText = message;
-            container.appendChild(error);
-        }
+    let error = container.querySelector(".error-message");
+    if (error) {
+      container.removeChild(error);
     }
-    function validateEmail() {
-        let value = emailInput.value;
+    if (message) {
+      let error = document.createElement("div");
+      error.classList.add("error-message");
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  }
+  function validateEmail() {
+    let value = emailInput.value;
 
-        if (!value) {
-            showErrorMessage(emailInput, 'Email is a required field.');
-            return false;
-        }
-        if (value.indexOf('@') === -1) {
-            showErrorMessage(emailInput, 'You must enter a valid Email.');
-            return false;
-        }
-        if (value.indexOf('.') === -1) {
-            showErrorMessage(emailInput, 'You must enter a valid Email.');
-            return false;
-        }
-        showErrorMessage(emailInput, null) 
-            return true;
+    if (!value) {
+      showErrorMessage(emailInput, "Email is a required field.");
+      return false;
     }
-    function validateMessage() {
-        let value = messageInput.value;
+    if (value.indexOf("@") === -1) {
+      showErrorMessage(emailInput, "You must enter a valid Email.");
+      return false;
+    }
+    if (value.indexOf(".") === -1) {
+      showErrorMessage(emailInput, "You must enter a valid Email.");
+      return false;
+    }
+    showErrorMessage(emailInput, null);
+    return true;
+  }
+  function validateMessage() {
+    let value = messageInput.value;
 
-        if (!value) {
-            showErrorMessage(messageInput, 'Message is a required field.');
-            return false;
-        }
-        showErrorMessage(messageInput, null)
-        return true;
+    if (!value) {
+      showErrorMessage(messageInput, "Message is a required field.");
+      return false;
     }
-    function validateForm() {
-        let isValidEmail = validateEmail();
-        let isValidMessage = validateMessage();
-        return isValidEmail && isValidMessage;
+    showErrorMessage(messageInput, null);
+    return true;
+  }
+  function validateForm() {
+    let isValidEmail = validateEmail();
+    let isValidMessage = validateMessage();
+    return isValidEmail && isValidMessage;
+  }
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      alert("Success!");
     }
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        if (validateForm()) {
-            alert('Success!');
-        }
-    });
-    emailInput.addEventListener('input', validateEmail);
-    messageInput.addEventListener('input', validateMessage);
-})()
+  });
+  emailInput.addEventListener("input", validateEmail);
+  messageInput.addEventListener("input", validateMessage);
+
+
+})();
